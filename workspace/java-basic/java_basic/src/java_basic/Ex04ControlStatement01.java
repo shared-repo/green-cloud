6ae0be934d4ba2bfaa9b59b1632e6372bcff7baf
instructor <instructor@green.com>
2024-04-09 10:59:55 +0900

    update code

--- workspace/java-basic/java_basic/src/java_basic/Ex04ControlStatement01.java
+++ workspace/java-basic/java_basic/src/java_basic/Ex04ControlStatement01.java
@@ -26,7 +26,8 @@ public class Ex04ControlStatement01 {
 		// ------------------------------------
 		
 		// 4. 연산 수행 -> 결과를 변수에 저장
-		int result = 0;
+		double result = 0;
+		boolean valid = true;
 		if (op.equals("+")) { // 문자열의 내용을 비교할 때에는 반드시 equals 함수 사용 / == 비교연산자 사용 불가
 			result = operand1 + operand2;
 		} else if (op.equals("-")) {
@@ -34,13 +35,19 @@ public class Ex04ControlStatement01 {
 		} else if (op.equals("*")) {
 			result = operand1 * operand2;
 		} else if (op.equals("/")) {
-			result = operand1 / operand2;
-		} else {
+			// result = operand1 / operand2; 			// 정수 / 정수 -> 정수 ... 그러므로 소수점 데이터 손실
+			result = (double)operand1 / operand2; 		// 실수 / 정수 -> 실수
+		} else if (op.equals("%")) {
 			result = operand1 % operand2; // % : 나머지 연산자 -> 10 % 3 == 1
+		} else {
+			System.out.println("지원하지 않는 연산자 입니다.");
+			valid = false;
 		}
 		
 		// 5. 결과 출력
-		System.out.printf("%d %s %d = %d\n", operand1, op, operand2, result);
+		if (valid) {
+			System.out.printf("%d %s %d = %f\n", operand1, op, operand2, result);
+		}
 		
 
 	}
