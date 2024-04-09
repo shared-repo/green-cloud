a5e7b54cf85fe45dc7226814690884e633152223
instructor <instructor@green.com>
2024-04-09 14:46:58 +0900

    update code

--- workspace/java-basic/java_basic/src/java_basic/Ex04ControlStatement04.java
+++ workspace/java-basic/java_basic/src/java_basic/Ex04ControlStatement04.java
@@ -28,18 +28,18 @@ public class Ex04ControlStatement04 {
 		char grade;
 		boolean valid = true;
 		switch ((int)avg / 10) {
-		case 10: grade = 'A'; break;
+		case 10: 
 		case 9: grade = 'A'; break;
 		case 8: grade = 'B'; break;
 		case 7: grade = 'C'; break;
 		case 6: grade = 'D'; break;
-		case 5: grade = 'F'; break;
-		case 4: grade = 'F'; break;
-		case 3: grade = 'F'; break;
-		case 2: grade = 'F'; break;
-		case 1: grade = 'F'; break;
+		case 5: 
+		case 4: 
+		case 3: 
+		case 2: 
+		case 1: 
 		case 0: grade = 'F'; break;
-			default: valid = false;
+		default: valid = false; grade = 'X';
 		}
 		
 		// 4. 결과 출력
