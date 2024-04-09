9fe5c6514d7f05af78413627bbb648e8d9e24c2d
instructor <instructor@green.com>
2024-04-09 11:58:39 +0900

    update code

--- workspace/java-basic/java_basic/src/java_basic/Ex04ControlStatement02.java
+++ workspace/java-basic/java_basic/src/java_basic/Ex04ControlStatement02.java
@@ -1,6 +1,6 @@
 package java_basic;
 
-public class Ex04ControlStatement01 {
+public class Ex04ControlStatement02 {
 
 	public static void main(String[] args) {
 		
@@ -28,26 +28,20 @@ public class Ex04ControlStatement01 {
 		// 4. 연산 수행 -> 결과를 변수에 저장
 		double result = 0;
 		boolean valid = true;
-		if (op.equals("+")) { // 문자열의 내용을 비교할 때에는 반드시 equals 함수 사용 / == 비교연산자 사용 불가
-			result = operand1 + operand2;
-		} else if (op.equals("-")) {
-			result = operand1 - operand2;
-		} else if (op.equals("*")) {
-			result = operand1 * operand2;
-		} else if (op.equals("/")) {
-			// result = operand1 / operand2; 			// 정수 / 정수 -> 정수 ... 그러므로 소수점 데이터 손실
-			result = (double)operand1 / operand2; 		// 실수 / 정수 -> 실수
-		} else if (op.equals("%")) {
-			result = operand1 % operand2; // % : 나머지 연산자 -> 10 % 3 == 1
-		} else {
-			System.out.println("지원하지 않는 연산자 입니다.");
-			valid = false;
+		switch (op) {
+		case "+": result = operand1 + operand2; break;
+		case "-": result = operand1 - operand2; break;
+		case "*": result = operand1 * operand2; break;
+		case "/": result = operand1 / operand2; break;
+		case "%": result = operand1 % operand2; break;
+		default : valid = false;
 		}
 		
 		// 5. 결과 출력
 		if (valid) {
-			// System.out.print(operand1 + " " + op + " " + operand2 + " = " + result);
 			System.out.printf("%d %s %d = %f\n", operand1, op, operand2, result);
+		} else {
+			System.out.println("지원하지 않는 연산자입니다.");
 		}
 		
 
