ff74c21cfce72c2d0eca282f3cd5b7e862b2a361
instructor <instructor@green.com>
2024-04-11 11:22:34 +0900

    update code

--- workspace/java-basic/java_basic/src/java_basic/Ex04ControlStatement11.java
+++ workspace/java-basic/java_basic/src/java_basic/Ex04ControlStatement11.java
@@ -9,40 +9,51 @@ public class Ex04ControlStatement11 {
 		// 성적 처리기 만들기
 		java.util.Scanner scanner = new java.util.Scanner(System.in);
 		
-		System.out.print("첫 번째 점수 : ");
-		int score1 = scanner.nextInt();
-		System.out.print("두 번째 점수 : ");
-		int score2 = scanner.nextInt();
-		System.out.print("세 번째 점수 : ");
-		int score3 = scanner.nextInt();
+		while (true) { // 성적 처리기 반복
 		
-		int sum = score1 + score2 + score3;
-		double avg = sum / 3.;
+			System.out.print("첫 번째 점수 : ");
+			int score1 = scanner.nextInt();
+			System.out.print("두 번째 점수 : ");
+			int score2 = scanner.nextInt();
+			System.out.print("세 번째 점수 : ");
+			int score3 = scanner.nextInt();
+			
+			int sum = score1 + score2 + score3;
+			double avg = sum / 3.;
+			
+			char grade;
+			boolean valid = true;
+			switch ((int)avg / 10) {
+			case 10: 
+			case 9: grade = 'A'; break;
+			case 8: grade = 'B'; break;
+			case 7: grade = 'C'; break;
+			case 6: grade = 'D'; break;
+			case 5: 
+			case 4: 
+			case 3: 
+			case 2: 
+			case 1: 
+			case 0: grade = 'F'; break;
+			default: valid = false; grade = 'X';
+			} // end of switch
+			
+			// 4. 결과 출력
+			if (valid) {
+				System.out.printf("[SCORE1: %d][SCORE2: %d][SCORE3: %d][SUM: %d][AVG: %f][GRADE: %s]", 
+								  score1, score2, score3, sum, avg, grade);
+			} else {
+				System.out.println("입력 오류");
+			}
+			
+			System.out.print("계속할까요?(y or n) : ");
+			String again = scanner.next();
+			if (!again.equalsIgnoreCase("y")) { // !again.equalsIgnoreCase("y") == again.equalsIgnoreCase("y") == false
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			}
 		
-		char grade;
-		boolean valid = true;
-		switch ((int)avg / 10) {
-		case 10: 
-		case 9: grade = 'A'; break;
-		case 8: grade = 'B'; break;
-		case 7: grade = 'C'; break;
-		case 6: grade = 'D'; break;
-		case 5: 
-		case 4: 
-		case 3: 
-		case 2: 
-		case 1: 
-		case 0: grade = 'F'; break;
-		default: valid = false; grade = 'X';
-		}
-		
-		// 4. 결과 출력
-		if (valid) {
-			System.out.printf("[SCORE1: %d][SCORE2: %d][SCORE3: %d][SUM: %d][AVG: %f][GRADE: %s]", 
-							  score1, score2, score3, sum, avg, grade);
-		} else {
-			System.out.println("입력 오류");
-		}
+		} // end of while
 		
 
 	}
