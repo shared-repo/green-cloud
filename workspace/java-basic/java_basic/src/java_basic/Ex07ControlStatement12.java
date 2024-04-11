777569cfb5d802e895c78746c56087a57f9bf5fa
instructor <instructor@green.com>
2024-04-11 17:14:46 +0900

    update code

--- workspace/java-basic/java_basic/src/java_basic/Ex07ControlStatement12.java
+++ workspace/java-basic/java_basic/src/java_basic/Ex07ControlStatement12.java
@@ -11,9 +11,17 @@ public class Ex07ControlStatement12 {
 			
 			numbers[i] = (int)(Math.random() * 45) + 1; // 1 ~ 45, random
 			//중복 검사
+			for (int j = 0; j < i; j++) {
+				if (numbers[i] == numbers[j]) { // 중복인 경우
+					//i--;	// 현재 위치에서 다시 뽑기
+					i = -1;	// 처음부터 다시 뽑기
+					break;
+				}
+			}
 		}
 		
-		// 2. 
+		// 2. 뽑힌 숫자의 평균 값이 20 ~ 26 범위에 있는지 검사 -> 범위를 벗어나면 1부터 다시
+		
 		// 3.
 		// 4. 결과 출력
 		System.out.print("당첨 예상 번호 : ");
