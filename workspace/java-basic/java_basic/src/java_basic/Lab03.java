4943f656b597abcadf4eb6d294156c67c7d7f4ab
instructor <instructor@green.com>
2024-04-16 14:59:35 +0900

    update code

--- workspace/java-basic/java_basic/src/java_basic/Lab03.java
+++ workspace/java-basic/java_basic/src/java_basic/Lab03.java
@@ -5,10 +5,52 @@ package java_basic;
 
 public class Lab03 {
 
+	private static java.util.Scanner scanner = new java.util.Scanner(System.in);
+	
 	public static void main(String[] args) {
 		
 		// 2. 연락처 인스턴스 만들기 및 사용 실습 코드
-
+//		Contact contact;
+//		contact = new Contact(1, "장동건", "010-6542-8852", "jdk@example.com");
+//		System.out.println(contact.info());
+//		
+//		Contact contact2 = new Contact();
+//		contact2.setNo(2);
+//		contact2.setName("김윤석");
+//		contact2.setPhone("010-7412-9586");
+//		contact2.setEmail("kys@example.com");		
+//		System.out.println(contact2.info());
+		
+		program : while (true) {
+			String task = selectTask();
+			
+			switch (task) {
+			case "1": break;
+			case "2": break;
+			case "3": break;
+			case "4": break;
+			case "5": break;
+			case "0": 
+				System.out.println("프로그램을 종료합니다.");
+				break program;
+			default: 
+				System.out.println("지원하지 않는 작업입니다.");;
+			}
+		}
+	}
+	
+	static String selectTask() {
+		System.out.println("********************");
+		System.out.println("* 1. 연락처 등록");
+		System.out.println("* 2. 연락처 수정");
+		System.out.println("* 3. 연락처 삭제");
+		System.out.println("* 4. 연락처 검색");
+		System.out.println("* 5. 연락처 목록");
+		System.out.println("* 0. 종료");
+		System.out.println("********************");
+		System.out.print("작업을 선택하세요 : ");
+		String task = scanner.nextLine();
+		return task;
 	}
 
 }
@@ -18,3 +60,70 @@ public class Lab03 {
 //    메서드 : 연락처 정보 반환
 //    생성자 : 전달인자없는 생성자, 모든 필드를 전달인자로 하는 생성자
 //    필드 은닉화 처리 : private으로 만들고 getter, setter 만들기
+class Contact {
+	private int no;
+	private String name;
+	private String phone;
+	private String email;
+
+	public Contact() {}
+	public Contact(int no, String name, String phone, String email) {
+		this.no = no;
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+	}
+
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String info() {
+		return String.format("[%03d][%10s][%13s][%s]", no, name, phone, email);
+	}
+	  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
