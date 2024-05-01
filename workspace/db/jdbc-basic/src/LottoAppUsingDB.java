554f9a97e9b77aa97aeb519c2e947d8cd2733791
instructor <instructor@green.com>
2024-05-01 15:39:06 +0900

    update code

--- workspace/db/jdbc-basic/src/LottoAppUsingDB.java
+++ workspace/db/jdbc-basic/src/LottoAppUsingDB.java
@@ -1,5 +1,9 @@
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
 public class LottoAppUsingDB {
@@ -32,9 +36,40 @@ public class LottoAppUsingDB {
 			case "4": // 당첨 번호 데이터베이스 초기화
 				// market_db 데이터베이스에 테이블 만들기 ( mysql workbench에서 한 번만 수행, 테이블은 파일의 내용을 참고해서 구현 -- 아래 주석 참고 )
 				// 데이터베이스의 기존 데이터 모두 삭제 ( delete 구문 )
-				
 				// 파일에서 데이터 읽기 ( 1 ~ 1116회차 데이터 )
 				// 읽은 데이터를 데이터베이스에 저장
+				Connection conn = null;
+				PreparedStatement pstmt = null;				
+				try {
+					// 1. 드라이버 준비
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					
+					// 2. 연결 객체 만들기
+					conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/market_db", "green_cloud", "mysql");
+					
+					// 3-1. 명령 객체 만들기 (삭제 관련)
+					String sql = "DELETE FROM lotto_winning_number";
+					// String sql = "TRUNCATE TABLE lotto_winning_numbers;";
+					pstmt = conn.prepareStatement(sql);
+					
+					// 4-1. 명령 실행 ( 삭제 관련 )
+					pstmt.executeUpdate(); 
+					
+					// 3-2. 삽입 명령 객체 
+					
+					// 4-2. 삽입 실행
+					
+					
+					// 5. 결과가 있으면 결과 처리
+					
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				} finally {
+					// 6. 연결 종료					
+					try { pstmt.close(); } catch (Exception ex) {}
+					try { conn.close(); } catch (Exception ex) {}
+				}
+				
 				break;
 			case "5":
 				
