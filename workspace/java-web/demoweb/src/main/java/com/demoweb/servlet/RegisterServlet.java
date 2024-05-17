effe7578b0abcb43fb57d8f3aa405f370dee011e
instructor <instructor@green.com>
2024-05-17 16:19:28 +0900

    update code

--- workspace/java-web/demoweb/src/main/java/com/demoweb/servlet/RegisterServlet.java
+++ workspace/java-web/demoweb/src/main/java/com/demoweb/servlet/RegisterServlet.java
@@ -1,6 +1,9 @@
 package com.demoweb.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -34,36 +37,33 @@ public class RegisterServlet extends HttpServlet {
 		
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+
 		try {
 			// 1. 드라이버 준비
-			// DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
 			// 2. 연결 객체 만들기
-			conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/employees", "green_cloud", "mysql");
+			conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/demoweb", "green_cloud", "mysql");
 			
 			// 3. 명령 객체 만들기
-			String sql = "SELECT emp_no, first_name, last_name, gender, hire_date FROM employees LIMIT 100";
+			String sql = "INSERT INTO member (memberid, passwd, email) VALUES (?, ?, ?)";
 			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, memberId);
+			pstmt.setString(2, passwd);
+			pstmt.setString(3, email);
 			
 			// 4. 명령 실행 ( 결과가 있으면 결과 저장 - select 인 경우 )
-			rs = pstmt.executeQuery();
+			pstmt.executeUpdate(); // insert, update, delete sql은 executeUpdate로 실행
 			
 			// 5. 결과가 있으면 결과 처리
-			while (rs.next()) {
-				System.out.printf("[%d][%s][%s][%s][%s]\n", 
-						rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getDate(5));
-			}
+			
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		} finally {
 			// 6. 연결 종료
-			try { rs.close(); } catch (Exception ex) {}
 			try { pstmt.close(); } catch (Exception ex) {}
 			try { conn.close(); } catch (Exception ex) {}
 		}
-		
 		
 		// 3. login으로 이동 ( 다른 서블릿으로 이동 -> redirect로 이동 )
 		//    jsp로 이동 ( forward로 이동 )
