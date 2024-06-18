36e01d7fd343ee74a719359a161a6da36535fdde
instructor <instructor@green.com>
2024-06-18 17:19:04 +0900

    update code;

--- workspace/java-web/demoweb/src/main/java/com/demoweb/service/AppSettingsService.java
+++ workspace/java-web/demoweb/src/main/java/com/demoweb/service/AppSettingsService.java
@@ -1,17 +1,42 @@
 package com.demoweb.service;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import com.demoweb.dao.AppSettingsDao;
 import com.demoweb.dao.MySqlAppSettingsDao;
 import com.demoweb.dao.OracleAppSettingsDao;
 import com.demoweb.dto.AppSetting;
+import com.demoweb.factory.DemoWebBeanFactory;
 
 public class AppSettingsService {
 	
+	// 1. Class 사용 + new 사용
 	// private MySqlAppSettingsDao dao = new MySqlAppSettingsDao();
 	// private OracleAppSettingsDao dao = new OracleAppSettingsDao();
 	
+	// 2. interface 사용 + new 사용
 	// private AppSettingsDao dao = new MySqlAppSettingsDao();
-	private AppSettingsDao dao = new OracleAppSettingsDao();
+	// private AppSettingsDao dao = new OracleAppSettingsDao();
+	
+	// 3. interface 사용 + factory 사용
+	// AppSettingsDao dao = DemoWebBeanFactory.getSettingsDao("mysql");
+	// AppSettingsDao dao = DemoWebBeanFactory.getSettingsDao("oracle");
+	
+	// 4. interface 사용 + factory 사용 + 설정 파일 사용
+	AppSettingsDao dao;
+	public AppSettingsService() {
+		Properties props = new Properties();
+		try {
+			String path = "D:\\instructor-och\\green-cloud\\workspace\\java-web\\demoweb\\src\\main\\webapp\\WEB-INF\\config\\bean-config.properties";
+			FileInputStream fis = new FileInputStream(path);
+			props.load(fis);
+			String dsn = (String)props.get("app.settings.dsn");
+			dao = DemoWebBeanFactory.getSettingsDao(dsn);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 	
 	public AppSetting findAppSettingBySettingName(String settingName) {
 		AppSetting appSetting = dao.selectAppSettingBySettingName(settingName);
@@ -23,3 +48,9 @@ public class AppSettingsService {
 	}
 
 }
+
+
+
+
+
+
