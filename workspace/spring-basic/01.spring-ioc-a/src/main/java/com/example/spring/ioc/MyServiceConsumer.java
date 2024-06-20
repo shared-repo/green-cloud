08b9578c1123b3199a65b8d9c0126f1c2d61a4a6
instructor <instructor@green.com>
2024-06-20 16:25:01 +0900

    update code

--- workspace/spring-basic/01.spring-ioc-a/src/main/java/com/example/spring/ioc/MyServiceConsumer.java
+++ workspace/spring-basic/01.spring-ioc-a/src/main/java/com/example/spring/ioc/MyServiceConsumer.java
@@ -1,9 +1,22 @@
 package com.example.spring.ioc;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class MyServiceConsumer implements ServiceConsumer {
 
-	private TimeService timeService = new MyTimeService(); 
-	private MessageService messageService = new MyMessageService();	
+	// 1. 직접 인스턴스 생성 ( new 사용 )
+	// private TimeService timeService = new MyTimeService(); 
+	// private MessageService messageService = new MyMessageService();
+	
+	// 2. IoC 컨테이너를 사용해서 인스턴스 만들기
+	private TimeService timeService;
+	public void setTimeService(TimeService timeService) {
+		this.timeService = timeService;
+	}
+	private MessageService messageService;
+	public void setMessageService(MessageService messageService) {
+		this.messageService = messageService;
+	}
 
 	public void doSomething() {
 		String message = messageService.getMessage();
--- workspace/spring-basic/01.spring-ioc-a/src/main/java/com/example/spring/ioc/TestMain.java
+++ workspace/spring-basic/01.spring-ioc-a/src/main/java/com/example/spring/ioc/TestMain.java
@@ -1,10 +1,20 @@
 package com.example.spring.ioc;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class TestMain {
 
 	public static void main(String[] args) {
 		
-		MyServiceConsumer serviceConsumer = new MyServiceConsumer();
+		// 1. 직접 인스턴스 생성 ( new 사용 )
+		// ServiceConsumer serviceConsumer = new MyServiceConsumer();
+		
+		// 2. IoC 컨테이너를 사용해서 인스턴스 생성
+		GenericXmlApplicationContext appContext = new GenericXmlApplicationContext("app-context.xml");
+		// app-context.xml 설정 파일에서 id=serviceConsumer로 등록된 클래스의 인스턴스 반환
+		ServiceConsumer serviceConsumer = appContext.getBean("serviceConsumer", ServiceConsumer.class);
+		
+		
 		serviceConsumer.doSomething();
 
 	}
