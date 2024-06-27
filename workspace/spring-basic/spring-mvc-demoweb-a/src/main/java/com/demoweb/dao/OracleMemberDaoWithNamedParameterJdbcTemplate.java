84205d488d8864c6a964f6097420e10372b1dc3d
instructor <instructor@green.com>
2024-06-27 11:49:24 +0900

    update code

--- workspace/spring-basic/spring-mvc-demoweb-a/src/main/java/com/demoweb/dao/OracleMemberDaoWithNamedParameterJdbcTemplate.java
+++ workspace/spring-basic/spring-mvc-demoweb-a/src/main/java/com/demoweb/dao/OracleMemberDaoWithNamedParameterJdbcTemplate.java
@@ -5,12 +5,16 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.JdbcUtils;
 
 import com.demoweb.dto.MemberDto;
@@ -25,19 +29,29 @@ public class OracleMemberDaoWithNamedParameterJdbcTemplate implements MemberDao 
 	// 회원가입 처리 -> 회원정보를 데이터베이스에 저장
 	public void insertMember(MemberDto member) {
 		
-		String sql = "INSERT INTO member (memberid, passwd, email) VALUES (?, ?, ?)";
-		jdbcTemplate.update(sql, member.getMemberId(), member.getPasswd(), member.getEmail());
-			
+		String sql = "INSERT INTO member (memberid, passwd, email) VALUES (:memberId, :passwd, :email)";
+
+//		Map<String, Object> params = new HashMap<>();
+//		params.put("memberId", member.getMemberId());
+//		params.put("passwd", member.getPasswd());
+//		params.put("email", member.getEmail());
+//		jdbcTemplate.update(sql, params);
+		
+		SqlParameterSource params = new BeanPropertySqlParameterSource(member); // member의 getMethod로 자동 연결
+		jdbcTemplate.update(sql, params);
 	}
 	
 	public MemberDto selectMemberByMemberIdAndPasswd(MemberDto member) {
 		
 		String sql = "SELECT memberid, email, usertype, regdate, active " +
 					 "FROM member " +
-					 "WHERE memberid = ? AND passwd = ? ";
+					 "WHERE memberid = :memberId AND passwd = :passwd ";
+		
+		SqlParameterSource params = new BeanPropertySqlParameterSource(member);
 		
 		MemberDto selectedMember = 
 			jdbcTemplate.queryForObject(sql, 
+										params,
 										// 1. RowMapper 인터페이스를 구현하는 이름 없는 클래스 만들기 + 2. 그 클래스의 인스턴스 만들기
 										new RowMapper<MemberDto>() {	
 											@Override
@@ -50,8 +64,7 @@ public class OracleMemberDaoWithNamedParameterJdbcTemplate implements MemberDao 
 												member.setActive(rs.getBoolean(5));
 												return member;
 											}
-										}, 
-										member.getMemberId(), member.getPasswd());
+										});
 		
 		return selectedMember; // 조회 결과를 저장한 객체 반환
 		
@@ -59,8 +72,9 @@ public class OracleMemberDaoWithNamedParameterJdbcTemplate implements MemberDao 
 	
 	public void updatePasswd(MemberDto member) {
 		
-		String sql = "UPDATE member SET passwd = ? WHERE memberid = ?";
-		jdbcTemplate.update(sql, member.getPasswd(), member.getMemberId());
+		String sql = "UPDATE member SET passwd = :passwd WHERE memberid = :memberId";
+		SqlParameterSource params = new BeanPropertySqlParameterSource(member);
+		jdbcTemplate.update(sql, params);
 		
 	}
 
@@ -68,17 +82,19 @@ public class OracleMemberDaoWithNamedParameterJdbcTemplate implements MemberDao 
 
 		String sql = "SELECT COUNT(memberId) " +
 					 "FROM member " +
-					 "WHERE memberid = ? ";
-		
-//		int count = jdbcTemplate.queryForObject(sql, new Object[] {memberId}, Integer.class);		
-		int count = jdbcTemplate.queryForObject(sql, 
+					 "WHERE memberid = :memberId ";
+
+		Map<String, Object> params = new HashMap<>();
+		params.put("memberId", memberId);
+	
+		int count = jdbcTemplate.queryForObject(sql,
+												params,
 												new RowMapper<Integer>() {
 													@Override
 													public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
 														return rs.getInt(1);
 													}
-												},
-												memberId);
+												});
 			
 		return count; // 조회 결과를 저장한 변수 반환
 	}
