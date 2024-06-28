edeca70e04a3076a267a730edbae14356f8d0183
instructor <instructor@green.com>
2024-06-28 18:22:28 +0900

    update code

--- workspace/spring-basic/spring-mvc-demoweb-a2/src/main/java/com/demoweb/mapper/MemberMapper.java
+++ workspace/spring-basic/spring-mvc-demoweb-a2/src/main/java/com/demoweb/mapper/MemberMapper.java
@@ -1,9 +1,12 @@
 package com.demoweb.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.demoweb.dto.MemberDto;
 
 // MyBatis Mapper.xml 파일과 연동해서 동작하는 DAO 객체 생성할 때 적용할 인터페이스
 // Mapper.xml 파일과 동일하게 작성 (경로, 이름, 전달인자, 결과형, ... )
+@Mapper // root-context.xml 파일의 <bean id="memberMapper" ... 을 대신하는 annotation
 public interface MemberMapper {
 
 	void insertMember(MemberDto member);
