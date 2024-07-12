c6f18dc7c81cd8f7027929601d5f83d51bcd7400
instructor <instructor@green.com>
2024-07-12 16:35:21 +0900

    update code

--- intellij-workspace/demoweb3/src/main/java/com/demoweb/repository/BoardRepository.java
+++ intellij-workspace/demoweb3/src/main/java/com/demoweb/repository/BoardRepository.java
@@ -10,10 +10,6 @@ import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
 
-//    @Query(value =  "SELECT attachNo, userFileName, savedFileName, downloadCount " +
-//                    "FROM tbl_boardattach " +
-//                    "WHERE attachno = :attachNo ",
-//            nativeQuery = true) // 실제 데이터베이스의 구문 규칙에 따라 작성한 SQL
     @Query(value = "SELECT ba FROM BoardAttachEntity ba WHERE ba.attachNo = :attachNo")
     BoardAttachEntity findBoardAttachByAttachNo(@Param("attachNo") int attachNo);
 }
