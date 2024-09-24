cf9608c375d3ac404f4f4caeb4e17e42b57a2036
instructor <instructor@green.com>
2024-09-24 12:04:04 +0900

    update code

--- workspace/python-basic/chatbot-server/models/chatbot_models.py
+++ workspace/python-basic/chatbot-server/models/chatbot_models.py
@@ -1,5 +1,13 @@
 from pydantic import BaseModel # 유효성 검사 기능을 포함하는 DTO base 클래스
+from typing import List
 
 class ChatMessage(BaseModel): # BaseModel 클래스를 상속하는 Todo 클래스 
     message: str
+
+class ChatMessage2(BaseModel):
+    role: str
+    content: str
+
+class ChatMessageList(BaseModel):
+    messages: List[ChatMessage2]
 
--- workspace/python-basic/chatbot-server/routes/chatbot_route.py
+++ workspace/python-basic/chatbot-server/routes/chatbot_route.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from openai import OpenAI
 import os
 
-from models.chatbot_models import ChatMessage
+from models.chatbot_models import ChatMessage, ChatMessageList
 
 chatbot_router = APIRouter()
 
@@ -18,6 +18,24 @@ async def process_chat_text(msg: ChatMessage):
             {'role': 'system', 'content': 'You are an assistant that explain general topic. You have to answere as possible as short.'},
             {'role': 'user', 'content': msg.message}
         ],
+        n=1, 
+        temperature=1
+    )
+
+    return {
+        "metadata": "chatbot response",
+        "message": completion.choices[0].message.content
+    }
+
+@chatbot_router.post("/chat-text-with-history")
+async def process_chat_text_with_history(message_list: ChatMessageList):
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {'role': 'system', 'content': 'You are an assistant that explain general topic. You have to answere as possible as short.'}
+        # ] + message_list.messages,
+        ] + [{ 'role': message.role, 'content': message.content } for message in message_list.messages],
         n=1, 
         temperature=1
     )
