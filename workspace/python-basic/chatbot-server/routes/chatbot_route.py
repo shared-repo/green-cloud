e699118c5b7a322e1660d77d738448b2452921e8
instructor <instructor@green.com>
2024-09-27 11:03:28 +0900

    update code

--- workspace/python-basic/chatbot-server/routes/chatbot_route.py
+++ workspace/python-basic/chatbot-server/routes/chatbot_route.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File
 
 from openai import OpenAI
 import os
+import shutil
 
 from models.chatbot_models import ChatMessage, ChatMessageList
 from db import chromadb_helper
@@ -50,8 +51,8 @@ async def process_chat_text_with_history(message_list: ChatMessageList):
 @chatbot_router.post("/search-jobs")
 async def process_searching_jobs(message_list: ChatMessageList):
 
-    query = message_list.messages[-1].content # 마지막 요소 반환
-    selected_documents = chromadb_helper.query_similar_documents(query, top_k=10)
+    query = message_list.messages[-1].content # 마지막 질문 요소 반환
+    selected_documents = chromadb_helper.query_similar_documents(query, top_k=10) # vectordb에서 질문과 관련된 데이터 조회
 
     selected_documents2 = \
         [ [doc, dist] for (doc, dist) in zip(selected_documents['documents'][0], selected_documents['distances'][0]) ]
@@ -63,9 +64,8 @@ async def process_searching_jobs(message_list: ChatMessageList):
                 당신은 아래 지시사항을 적용해서 고객의 요청을 분석하고 적절한 답변을 제공해야 합니다.
 
                 [지시사항]
-                1. 채용공고 관련 요청인 경우 주어진 [채용공고 목록]을 기반으로 적합한 채용공고를 찾아주어야 합니다. 이 때 답변 형식은 번호. 회사이름, 타이틀 형식으로 만들어야 합니다.
-
-                2. 위의 지시사항에 해당되지 않는 경우 다른 정보에 의존하지 않고당신의 기본 지식을 사용해서 답변을 만들어야 합니다.이 때 답변 형식은 번호를 사용하지 않고 자유 문장형식으로 만들어야 합니다.
+                지시사항 1. 채용공고 관련 요청인 경우 주어진 [채용공고 목록]을 기반으로 적합한 채용공고를 찾아주어야 합니다. 답변에는 회사이름과 채용공고 제목만 포함해야 합니다.
+                지시사항 2. 위의 지시사항에 해당되지 않는 경우 당신의 기본 지식을 사용해서 답변을 만들어야 합니다.
 
                 ----------------------------------------------
                 
@@ -80,7 +80,7 @@ async def process_searching_jobs(message_list: ChatMessageList):
         messages=[
             {'role': 'system', 'content': context_str }
         # ] + message_list.messages,
-        ] + [{ 'role': message.role, 'content': message.content } for message in message_list.messages],
+        ] + [{ 'role': message.role, 'content': message.content } for message in message_list.messages[-1:]],
         n=1, 
         temperature=1
     )
@@ -90,4 +90,19 @@ async def process_searching_jobs(message_list: ChatMessageList):
         "message": completion.choices[0].message.content
     }
 
+
+@chatbot_router.post("/audio-to-text")
+async def process_audio(audio: UploadFile = File(...)) -> str :
+    print( "------------------->", audio.filename )
+
+    with open(audio.filename, "wb") as f:
+        shutil.copyfileobj(audio.file, f) # 파일로 저장
+
+    with open(audio.filename, "rb") as f:
+        transcription = client.audio.transcriptions.create(
+            model='whisper-1',
+            file=f
+        )
+
+    return transcription.text
 
